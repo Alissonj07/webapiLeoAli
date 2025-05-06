43871bb66536871733cd87a2c93069ff86862469
Leonardo <leonardo.rambo@cs.cruzeirodosul.edu.br>
2025-05-05 21:39:06 -0300

    Final

--- Controllers/EventosController.cs
+++ Controllers/EventosController.cs
@@ -2,14 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoAliLeo.Data;
+using ProjetoAliLeo.Models;
 
 namespace ProjetoAliLeo.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
+        [ApiController]
+        [Route("api/[controller]")]
+
     public class EventosController : ControllerBase
+{
+    private readonly IEventoRepository _eventoRepository;
+     public EventosController(IEventoRepository eventoRepository)
     {
-        
+        _eventoRepository = eventoRepository;
+    }
+
+    
+        [HttpPost("cadastrar")]
+        public IActionResult Cadastrar([FromBody] Evento evento)
+        {
+            _eventoRepository.Cadastrar(evento);
+            return Created("", evento);
+        }
+
+        [HttpGet("listar")]
+        [Authorize]
+        public IActionResult Listar()
+        {
+            var eventos = _eventoRepository.Listar();
+            return Ok(eventos);
+        }
     }
 }
